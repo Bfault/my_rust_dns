543815fb904c967bd1b19268646c354c54ca554d
lorenzo <lorenzo.carrascosa@epitech.eu>
2023-03-11 19:18:52 +0900

    dump server done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::io::Result;
 use std::net::UdpSocket;
 
@@ -10,24 +9,24 @@ mod question;
 mod rescode;
 
 use packet::{BytePacketBuffer, Packet};
+use question::Question;
+use rescode::ResultCode;
 
 use crate::query::QueryType;
 
-fn main() -> Result<()> {
-    let qname = env::args().nth(1).unwrap_or("google.com".to_string());
-    let qtype = QueryType::MX;
-
+fn lookup(qname: &str, qtype: QueryType) -> Result<Packet> {
     let server = ("8.8.8.8", 53);
+
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
     let mut packet = Packet::new();
 
-    packet.header.id = 666;
+    packet.header.id = 6666;
     packet.header.qdcount = 1;
     packet.header.rd = true;
-    packet.questions.push(
-        question::Question::new(qname.to_string(), qtype)
-    );
+    packet
+        .questions
+        .push(Question::new(qname.to_string(), qtype));
 
     let mut req_buffer = BytePacketBuffer::new();
     packet.write(&mut req_buffer)?;
@@ -36,21 +35,67 @@ fn main() -> Result<()> {
     let mut res_buffer = BytePacketBuffer::new();
     socket.recv_from(&mut res_buffer.buf)?;
 
-    let res_packet = Packet::from_buffer(&mut res_buffer)?;
-    println!("{:#?}", res_packet.header);
+    Packet::from_buffer(&mut res_buffer)
+}
 
-    for q in res_packet.questions {
-        println!("{:#?}", q);
+fn handle_query(socket: &UdpSocket) -> Result<()> {
+    let mut req_buffer = BytePacketBuffer::new();
+
+    let (_, src) = socket.recv_from(&mut req_buffer.buf)?;
+
+    let mut request = Packet::from_buffer(&mut req_buffer)?;
+
+    let mut packet = Packet::new();
+    packet.header.id = request.header.id;
+    packet.header.rd = true;
+    packet.header.ra = true;
+    packet.header.qr = true;
+
+    if let Some(question) = request.questions.pop() {
+        println!("Received query: {:?}", question);
+
+        if let Ok(result) = lookup(&question.name, question.qtype) {
+            packet.questions.push(question);
+            packet.header.rcode = result.header.rcode;
+
+            for rec in result.answers {
+                println!("Answer: {:?}", rec);
+                packet.answers.push(rec);
+            }
+            for rec in result.authorities {
+                println!("Authority: {:?}", rec);
+                packet.authorities.push(rec);
+            }
+            for rec in result.resources {
+                println!("Resource: {:?}", rec);
+                packet.resources.push(rec);
+            }
+        } else {
+            packet.header.rcode = ResultCode::SERVFAIL;
+        }
     }
-    for rec in res_packet.answers {
-        println!("{:#?}", rec);
+    else {
+        packet.header.rcode = ResultCode::FORMERR;
     }
-    for rec in res_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.resources {
-        println!("{:#?}", rec);
-    }
+
+    let mut res_buffer = BytePacketBuffer::new();
+    packet.write(&mut res_buffer)?;
+
+    let len = res_buffer.pos();
+    let data = res_buffer.get_range(0, len)?;
+
+    socket.send_to(data, src)?;
 
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
+
+    loop {
+        match handle_query(&socket) {
+            Ok(_) => {},
+            Err(e) => eprintln!("An error occurred: {}", e),
+        }
+    }
 }
